1ca49f82425647e7ded90d93b42c40c3c924fd9b
MartinGLouw <ST10208767@vgconnect.edu.za>
2023-07-01 14:25:11 +0200

    DataBase display done

---
+++ GADE POE/Assets/Scripts/EndScreenScore.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndScreenScore : ScoreScript
+{
+    public TextMeshProUGUI ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.text = ("Score: " + ScoreScript.score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
